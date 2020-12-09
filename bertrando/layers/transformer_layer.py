3c3ee42886e20d72c4c7c7800cbe162b3c2f0582
Tomas Mikeska <tomikeska@gmail.com>
2020-12-09 20:54:51 +0100

    add ReZero transformer layer

--- bertrando/layers/transformer_layer.py
+++ bertrando/layers/transformer_layer.py
@@ -1,3 +1,4 @@
+import torch
 import torch.nn as nn
 from layers.ffn import PositionwiseFeedForward
 from layers.attention import MultiHeadedAttention
@@ -16,3 +17,20 @@ class TransformerEncoderLayer(nn.Module):
     def forward(self, x):
         x = self.norm1(x + self.self_attn(x))
         return self.norm2(x + self.ffn(x))
+
+
+class ReZeroTransformerLayer(nn.Module):
+
+    def __init__(self, d_model=512, n_heads=8, d_ff=2048, dropout=0.1):
+        super(ReZeroTransformerLayer, self).__init__()
+
+        self.norm1 = LayerNorm(d_model)
+        self.self_attn = MultiHeadedAttention(n_heads, d_model)
+        self.ffn = PositionwiseFeedForward(d_model, d_ff, dropout)
+        self.norm2 = LayerNorm(d_model)
+
+        self.resweight = nn.Parameter(torch.tensor(0.0))
+
+    def forward(self, x):
+        x = x + self.resweight * self.self_attn(x)
+        return x + self.resweight * self.ffn(x)
