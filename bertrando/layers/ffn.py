67a868048e52c56453a52d955fee401d9a75f3ff
Tomas Mikeska <tomikeska@gmail.com>
2021-02-15 16:58:20 +0100

    add GLU layer - alternative to FFN

--- bertrando/layers/ffn.py
+++ bertrando/layers/ffn.py
@@ -1,3 +1,4 @@
+import torch
 import torch.nn as nn
 
 
@@ -12,3 +13,35 @@ class PositionwiseFeedForward(nn.Module):
 
     def forward(self, x):
         return self.w_2(self.dropout(self.act(self.w_1(x))))
+
+
+class ChunkedFeedForward(nn.Module):
+
+    def __init__(self, d_model, d_ff, dropout=0.1, n_chunks=16):
+        super(ChunkedFeedForward, self).__init__()
+        self.w_1 = nn.Linear(d_model, d_ff)
+        self.w_2 = nn.Linear(d_ff, d_model)
+        self.dropout = nn.Dropout(dropout)
+        self.act = nn.GELU()
+        self.n_chunks = n_chunks
+
+    def forward_chunk(self, x):
+        return self.w_2(self.dropout(self.act(self.w_1(x))))
+
+    def forward(self, x):
+        chunked_x = x.chunk(self.n_chunks, dim=1)
+        chunked_y = [self.forward_chunk(x) for x in chunked_x]
+        return torch.cat(chunked_y, dim=1)
+
+
+class GLU(nn.Module):
+
+    def __init__(self, d_model, d_ff, dropout=0.1):
+        super().__init__()
+        self.w_1 = nn.Linear(d_model, d_ff, bias=False)
+        self.v_1 = nn.Linear(d_model, d_ff, bias=False)
+        self.w_2 = nn.Linear(d_ff, d_model, bias=False)
+        self.act = nn.GELU()
+
+    def forward(self, x, **kwargs):
+        return self.w_2(self.act(self.w_1(x)) * self.v_1(x))
